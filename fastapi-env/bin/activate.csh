edd93d1c6cceed49007a2ea0d281a4c7037bd8b8
Avni Girish <avni123.girish@gmail.com>
2025-01-04 21:10:21 -0500

    Renamed Project

--- fastapi-env/bin/activate.csh
+++ fastapi-env/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /Users/avnigirish/Documents/moodsync+/fastapi-env
+setenv VIRTUAL_ENV /Users/avnigirish/Documents/moodsync/fastapi-env
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 from fastapi import FastAPI
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = FastAPI()
 
+#Configure Spotify API
+spotify = Spotify(auth_manager=SpotifyClientCredentials(
+    client_id = "f524fa5d6eec4cfcb4c05f8469a612c0",
+    cleint_secret = "22cec1f60c3649ed984e5930b5869502"
+))
+
 @app.get("/")
 def index():
-    return {"message": "Hello, FastAPI World!"}
+    return {"message": "Welcome to MoodSync+"}
+
+@app.get("/spotify-recommendations/")
+def get_spotify_recommendations(mood: str):
+    try:
+        #Map moods to Spotify seach queries
+        mood_queries = {
+            "happy": "happy vibes",
+            "sad": "calm and relaxing",
+            "energetic": "workout beats",
+            "relaxed": "chill acoustic"
+        }
+
+        query = mood_queries(mood.lower(), "mood booster")
+        results = spotify.search(q=query, type="playlist", limit=3)
+
+        #Extract playlist data
+        playlists = [
+            {
+                "name": playlist["name"],
+                "description": playlist["description"],
+                "url": playlist["external_urls"]["spotify"]
+            }
+            for playlist in results["playlists"]["items"]
+        ]
+
+        return {"mood": mood, "playlists": playlists}
+    except Exception as e:
+        return {"error": str(e)}
+        
 
 #Analyze mood
 @app.post("/analyze_mood/")
